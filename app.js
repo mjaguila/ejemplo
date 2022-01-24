ea21e4e55f26063d22d05326e8d97c1fb4a069ec
mjaguila <mjaguila23@hotmail.com>
2022-01-24 00:51:00 -0300

    mensaje

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    //console.log(__dirname)
+    res.render('index', {titulo: "mi título dinámico"})
+})
+
+app.get('/servicios', (req, res) => {
+    res.render('servicios', {tituloServicios: "Título servicios"})
+})
+
+app.use((req, res, next) => {
+    res.status(404).render('404',{
+        titulo: "404",
+        descripcion: 'Titulo de mi web'    
+    })
+})
+
+app.listen(port, () => {
+    console.log('Servidor a su servicio en puerto', port)
+})
